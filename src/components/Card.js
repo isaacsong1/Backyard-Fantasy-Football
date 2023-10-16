bae184fd3ddb76ad9d78fdbfff584a437dc87f1d
Isaac Song <isaacsongs10@gmail.com>
2023-10-16 12:58:31 -0700

    Add CSS updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,21 +8,21 @@ function Card({player, handleRoster})  {
     setCheckPPR(currentPPR => !currentPPR)
   }
 
- 
+
   return (
 
     <div id='playerCard'>
         <h4 id="name">{player.name}</h4>
-         <h5 id="nickname">{player.nickname} : {player.position}</h5>
-        <img id='img' src={player.image}/>
-       
+        <h5 id="nickname">{player.nickname} : {player.position}</h5>
+        <img id='img' src={player.image} alt={player.name} />
+    
         <>
-        {checkPPR ? 
-        <div>
-          <p id="ppr">Projected PPR: {player.PPR_projected}</p>
-          <p id="ppr">Last Weeks PPR: {player.past_PPR}</p>
-        </div>
- :""}
+          {checkPPR ? 
+          <div>
+            <p id="ppr">Projected PPR: {player.PPR_projected}</p>
+            <p id="ppr">Last Weeks PPR: {player.past_PPR}</p>
+          </div> 
+          :""}
         </>
         <button id='btn' onClick={handlePPR}>See PPR</button>
         <button id='btn' onClick={() => handleRoster(player)}>Handle Roster</button>
