2b1541f4c9319bce8dbf33e584602acddb93b319
Isaac Song <isaacsongs10@gmail.com>
2023-10-16 12:29:13 -0700

    Change filter function, add filterBy state

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -6,6 +6,7 @@ import NavBar from "./NavBar";
 
 function CardContainer()  {
   const [players, setPlayers] = useOutletContext();
+  const [filterBy, setFilterBy] = useState("All");
 
   const handleSort = (e) => {
     const selectedValue = e.target.value; 
@@ -17,19 +18,18 @@ function CardContainer()  {
   }
 
   const handleFilter = (e) => {
-    const selectedValue = e.target.value;
-    if (selectedValue) {
-      setPlayers(currentPlayers => currentPlayers.filter(player => player.position === selectedValue))
-    } 
+    setFilterBy(e.target.value);
   }
 
-  const mappedPlayers = players.map(player => <Card key={player.id} player={player} />)
+  const mappedPlayers = players
+                        .filter(player => filterBy === "All" || player.position.toUpperCase() === filterBy.toUpperCase())
+                        .map(player => <Card key={player.id} player={player} />)
 
   return (
     <div>
       {/* <NavBar /> */}
       <h3>Football Players</h3>
-      <select onChange={handleFilter}>
+      <select onChange={handleFilter} value={filterBy} >
         <option value="All">Select a Position</option>
         <option value="QB">QB</option>
         <option value="RB">RB</option>
