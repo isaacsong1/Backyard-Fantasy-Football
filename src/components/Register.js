6dd5085850661bff9eaae36fd9e44abade2fb5e2
Wesley <wesmith314@gmail.com>
2023-10-17 18:49:20 -0700

    adds fetch

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useNavigate } from "react-router-dom"
 const userURL = "http://localhost:3000/users"
 
 
@@ -8,15 +9,56 @@ function Register() {
         password: "",
         confirm: ""
         });
+    const navigate = useNavigate()
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+            console.log(name, value)
+            setFormData({...formData, [name]: value})
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const { name, password } = formData
+        const newUser = {
+            name,
+            password
+        };
+        if (formData.password === formData.confirm) {
+            fetch(userURL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newUser)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setFormData({
+                    name: "",
+                    password: "",
+                    confirm: ""
+                })
+                navigate("/signin")
+            })
+        } else {
+            alert("Please confirm passwords match")
+        }
+    }
+
 
     return (
         <section>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Register Now!</h1>
                 <label htmlFor='name'>Username</label>
                 <br />
                 <input
-                    ype="name"
+                    onChange={handleChange}
+                    value={formData.name}
+                    type="name"
                     placeholder="User's Name"
                     id="name"
                     name="name">
@@ -25,6 +67,8 @@ function Register() {
                 <br /><label htmlFor='password'>Password</label>
                 <br />
                 <input
+                    onChange={handleChange}
+                    value={formData.password}
                     type="password"
                     placeholder='********'
                     id="password"
@@ -34,6 +78,8 @@ function Register() {
                 <br /><label htmlFor='confirm'>Confirm Password</label>
                 <br />
                 <input
+                    onChange={handleChange}
+                    value={formData.confirm}
                     type="password"
                     placeholder='********'
                     id="confirm"
@@ -44,7 +90,7 @@ function Register() {
                 <br />
             </form>
         </section>
-)
+    )
 }
 
 
